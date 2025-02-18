babb8f6f0de447249cf4e52442bb860fd4aa721d
Daniel Sayer <daniel.sayer01@gmail.com>
2025-02-18 22:57:07 +1000

    feat: add spotlight

--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -1,16 +1,18 @@
 import { Hero } from "./hero";
 import { Features } from "./features";
 import { Billing } from "./billing";
+import { Spotlight } from "@/components/ui/spotlight";
 
 export default function HomePage() {
   return (
-    <>
+    <div className="relative flex w-full flex-col overflow-hidden rounded-md bg-black/[0.96] antialiased md:items-center md:justify-center">
+      <Spotlight />
       <div className="mt-[1rem] flex w-full flex-col items-center justify-center p-3">
         <Hero />
       </div>
       <Features />
       <Billing />
       <div className="h-36"></div>
-    </>
+    </div>
   );
 }
