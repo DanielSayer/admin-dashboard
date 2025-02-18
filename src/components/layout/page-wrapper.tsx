0ada0dc437aa5ed90a2fba4c65d23e3a7d4953f5
Daniel Sayer <daniel.sayer01@gmail.com>
2025-02-18 23:02:32 +1000

    style: tidy up page height

--- src/components/layout/page-wrapper.tsx
+++ src/components/layout/page-wrapper.tsx
@@ -5,7 +5,7 @@ export default function PageWrapper() {
   return (
     <div className="[--header-height:calc(theme(spacing.20))]">
       <NavBar />
-      <main className="min-w-screen flex min-h-full flex-col items-center justify-between bg-white dark:bg-black">
+      <main className="min-w-screen flex min-h-[calc(100vh-var(--header-height)-1px)] flex-col items-center justify-between bg-white dark:bg-black">
         <div className="pointer-events-none absolute inset-0 z-[-99] flex items-center justify-center [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
         <Outlet />
       </main>
