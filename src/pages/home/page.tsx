79b3f85b835bb5875a757faeb3051e84ca1e64eb
Daniel Sayer <daniel.sayer01@gmail.com>
2025-02-18 23:00:34 +1000

    fix: dark mode on home page

--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -5,7 +5,7 @@ import { Spotlight } from "@/components/ui/spotlight";
 
 export default function HomePage() {
   return (
-    <div className="relative flex w-full flex-col overflow-hidden rounded-md bg-black/[0.96] antialiased md:items-center md:justify-center">
+    <div className="relative flex w-full flex-col overflow-hidden rounded-md bg-zinc-100 antialiased dark:bg-black/[0.96] md:items-center md:justify-center">
       <Spotlight />
       <div className="mt-[1rem] flex w-full flex-col items-center justify-center p-3">
         <Hero />
